a597797c744ddf9bb195e4aeaa3bb500f87d2bcf
Kevin Pancake <k3min@liquid8002.net>
2014-07-22 20:53:37 +0200

    Bugfixes

--- js/hashbang.js
+++ js/hashbang.js
@@ -3,7 +3,7 @@
 //     Hashbang may be freely distributed under the MIT license.
 
 
-(function(window) {
+(function (window) {
 
 	// Setup
 	// -----
@@ -13,9 +13,13 @@
 		location = window.location,
 
 		define = Object.defineProperty,
-		clone = Object.create,
+		clone = Object.create;
 
-		isIE = /MSIE 10/.test(navigator.appVersion);
+	// Test to check if Trident (IE).
+	var isTrident = function(n) {
+		var app = navigator.appVersion;
+		return /Trident/.test(app) && app.match(/Trident\/(\d)/)[1] <= n;
+	};
 
 	// Top-level namespace.
 	var HB = window.HB = {
@@ -30,7 +34,9 @@
 		collections: {},
 
 		// Call this to fire up Hashbang.
-		main: function(home, root) {
+		main: function (home, root) {
+
+			// Handles HTTP requests.
 			this.request = new Request(this.endpoint);
 
 			// Hashbang will go back to this hash is something goes wrong.
@@ -48,17 +54,18 @@
 			// Set up fallback in case `HB.router` doesn't know what to do.
 			this.router.fallback = this.fallback;
 
-			this.router.add("/:collection", function(c) {
+			// Main route.
+			this.router.add("/:collection", function (c) {
 				if (HB.collections[c] === undefined) {
 					HB.collections[c] = new Collection();
 					HB.collections[c].addEventListener("update", HB.update, false);
 
 					HB.request.get({
 						handle: c,
-						success: function(data) {
+						success: function (data) {
 							HB.collections[c].load(data);
 						},
-						error: function() {
+						error: function () {
 							delete HB.collections[c];
 							HB.fallback();
 						}
@@ -67,28 +74,34 @@
 					// TODO: Fix this ugly hack.
 					HB.collections[c].block = undefined;
 				}
-
-				while (HB.root.firstChild) {
-					HB.root.removeChild(HB.root.firstChild);
-				}
-
-				HB.root.appendChild(HB.collections[c]);
 			});
 
-			this.router.add("/:collection/:block", function(c, b) {
+			// Route for *subpages*.
+			this.router.add("/:collection/:block", function (c, b) {
 				HB.collections[c].block = b;
 			});
 
+			// Match routes for initial location.
 			this.router.match();
 		},
 
 		// This gets called when a collection updates.
-		update: function(e) {
-			document.title = HB.title.spec.format(HB.title.text, e.detail.title);
+		update: function (event) {
+
+			// Set the title.
+			document.title = HB.title.spec.format(HB.title.text, event.detail.title);
+
+			// Remove existing children.
+			while (HB.root.firstChild) {
+				HB.root.removeChild(HB.root.firstChild);
+			}
+
+			// Append current collection to `HB.root`.
+			HB.root.appendChild(HB.collections[event.detail.handle]);
 		},
 
 		// Function to call if collection is not found.
-		fallback: function() {
+		fallback: function () {
 			location.hash = HB.home;
 		}
 	};
@@ -98,7 +111,7 @@
 
 	// Create a new class. Add `$static` to a method name to make it *static*.
 	// `$hidden` makes it non-enumerable. You can even define getters and setters.
-	var klass = window.klass = function(methods) {
+	var klass = window.klass = function (methods) {
 		var base = methods.constructor;
 
 		// If extending...
@@ -145,9 +158,9 @@
 		var customElements = {};
 
 		// Function to make a custom element custom.
-		var register = function(e, p) {
-			if (isIE) {
-				Array.apply(null, Object.getOwnPropertyNames(p)).forEach(function(n) {
+		var register = function (e, p) {
+			if (isTrident(6)) {
+				Array.apply(null, Object.getOwnPropertyNames(p)).forEach(function (n) {
 					if (n !== "constructor") {
 						define(e, n, { value: p[n] });
 					}
@@ -158,7 +171,7 @@
 		};
 
 		// This is (almost) the same as the real deal.
-		document.registerElement = function(element, props) {
+		document.registerElement = function (element, props) {
 			customElements[element] = props;
 
 			var HTMLCustomElement = function HTMLCustomElement() {
@@ -179,12 +192,12 @@
 		};
 
 		// Make existing custom elements custom.
-		document.addEventListener("DOMContentLoaded", function() {
+		document.addEventListener("DOMContentLoaded", function () {
 			for (var i in customElements) {
 				var q = customElements[i]["extends"] ?
 					"{}[is={}]".format(customElements[i]["extends"], i) : i;
 
-				Array.apply(null, document.querySelectorAll(q)).forEach(function(e) {
+				Array.apply(null, document.querySelectorAll(q)).forEach(function (e) {
 					register(e, customElements[i].prototype);
 
 					if (e.attachedCallback !== undefined) {
@@ -200,7 +213,7 @@
 	// HB.Template
 	// -----------
 
-	// `window.HTMLTemplateElement` polyfill.
+	// `HTMLTemplateElement` polyfill.
 	if (window.HTMLTemplateElement === undefined) {
 		window.HTMLTemplateElement = HTMLElement.extend({
 			constructor: function HTMLTemplateElement() {
@@ -214,7 +227,7 @@
 		prototype: clone(window.HTMLTemplateElement.prototype, {
 
 			// *Constructor*.
-			attachedCallback: { value: function() {
+			attachedCallback: { value: function () {
 				var source = this.innerHTML;
 
 				source = source.replace(/\s{2,}/g, "");
@@ -227,7 +240,7 @@
 			}},
 
 			// This returns a HTML string.
-			render: { value: function(data) {
+			render: { value: function (data) {
 				return this.source.call(data);
 			}}
 		}),
@@ -242,14 +255,14 @@
 		prototype: clone(HTMLElement.prototype, {
 
 			// This method turns JSON into children.
-			load: { value: function(data) {
+			load: { value: function (data) {
 				this.id = data.id;
 				this.handle = data.handle;
 				this.title = data.title;
 				this.type = data.type;
 				this.template = window[data.type];
 
-				this.blocks = data.blocks.map(function(data) {
+				this.blocks = data.blocks.map(function (data) {
 					return new Block(data);
 				});
 
@@ -262,8 +275,8 @@
 
 			// This getter/setter gets/sets the current shown block.
 			block: {
-				get: function() { return this._block; },
-				set: function(block) {
+				get: function () { return this._block; },
+				set: function (block) {
 					this._block = block;
 
 					if (this.blocks !== undefined) {
@@ -274,7 +287,7 @@
 
 			// Sorts `this.blocks` by `by` and updates the collection.
 			// Uses `block.reduce`, and if `by` starts with a `-`, guess what?
-			sort: { value: function(by) {
+			sort: { value: function (by) {
 				var order = by[0] === "-" ?
 					(by = by.substr(1), -1) : 1;
 
@@ -301,17 +314,24 @@
 			}},
 
 			// Updates the collection.
-			update: { value: function() {
-				var data = HB.collection = this._block === undefined ?
+			update: { value: function () {
+
+				// Figure out what to use as `data`.
+				var data = this._block === undefined ?
 					this : this.search("handle", this._block);
 
+				// Object to give to the `update` event.
 				var detail = {
+					handle: this.handle,
 					title: this.title,
 					type: this.type,
 					template: this.template
 				};
 
+				// Check if `this.block` is defined so it can show *subpage*.
 				if (this._block !== undefined) {
+
+					// If so, but not found, go back to start.
 					if (data.blocks.length === 0) {
 						return (location.hash = "#!/" + this.handle);
 					}
@@ -321,14 +341,17 @@
 					detail.template = data.blocks[0].template;
 				}
 
+				// Halt if needed template isn't defined.
 				if (detail.template === undefined) {
 					return console.error("Template #{} is not defined".format(detail.type));
 				}
 
+				// Remove existing children.
 				while (this.firstChild) {
 					this.removeChild(this.firstChild);
 				}
 
+				// Turn HTML generated by `template.render` into children.
 				this.insertAdjacentHTML("beforeend", detail.template.render(data));
 
 				// Fires the event `update` when the collection updates.
@@ -353,7 +376,7 @@
 		},
 
 		// Returns the value of a property. Like `attributes.example`.
-		reduce: function(to) {
+		reduce: function (to) {
 			var find = to.split("."),
 				result = this[find[0]];
 
@@ -377,15 +400,15 @@
 			window.addEventListener("hashchange", this.match.bind(this), false);
 		},
 
-		// Adds a `route` — with corresponding `callback` — to the router.
-		add$hidden: function(route, callback) {
+		// Adds the `route` — with corresponding `callback` — to the router.
+		add$hidden: function (route, callback) {
 			this[route] = callback;
 			this[route].match = new RegExp(route.replace(/:\w+/g, "([\\w-]+)"));
 		},
 
 		// This gets called when `location.hash` changes,
 		// and tries to do something with the defined routes.
-		match$hidden: function() {
+		match$hidden: function () {
 			var fallback = true;
 
 			for (var i in this) {
@@ -428,7 +451,7 @@
 		// GET JSON from `this.endpoint` with specified `props`.
 		// (With `handle` being the most common)
 		// Calls `props.success` if all good, else `props.error`.
-		get: function(props) {
+		get: function (props) {
 			this.success = props.success;
 			this.error = props.error;
 
@@ -440,7 +463,7 @@
 		},
 
 		// Response to JSON.
-		load: function() {
+		load: function () {
 			var data = JSON.parse(this.xhr.response);
 
 			if (this.xhr.status === 200) {
@@ -457,7 +480,7 @@
 	// -------
 
 	// Formats a string (`:param` or `{n}` and/or `{}`).
-	String.prototype.format = function(data) {
+	String.prototype.format = function (data) {
 		var i = 0;
 
 		if (typeof data !== "object") {
@@ -465,13 +488,13 @@
 			data = [].slice.call(arguments);
 		}
 
-		return this.replace(/:(\w+)|\{([0-9])?\}/g, function(undefined, $1, $2) {
+		return this.replace(/:(\w+)|\{([0-9])?\}/g, function (undefined, $1, $2) {
 			return data[$1 || $2 || i++];
 		});
 	};
 
 	// It's kinda like `date` in PHP.
-	Date.prototype.format = function(spec) {
+	Date.prototype.format = function (spec) {
 		var date = this.getDate(),
 			day = this.getDay(),
 			month = this.getMonth(),
@@ -506,7 +529,7 @@
 			s: seconds < 10 ? "0" + seconds : seconds
 		};
 
-		return spec.replace(/\\?([a-z])/gi, function($0, $1) {
+		return spec.replace(/\\?([a-z])/gi, function ($0, $1) {
 			return options[$0] !== undefined ? options[$0] : $1;
 		});
 	};
@@ -539,7 +562,7 @@
 		"December"
 	];
 
-	// HTML5 `dataset` polyfill for IE10.
+	// `dataset` polyfill.
 	if (document.documentElement.dataset === undefined) {
 		define(Element.prototype, "dataset", {
 			get: function () {
@@ -559,7 +582,7 @@
 	}
 
 	// `CustomEvent` polyfill.
-	if (isIE) {
+	if (isTrident(7)) {
 		window.CustomEvent = Event.extend({
 			constructor: function CustomEvent(type, props) {
 				var event = document.createEvent("CustomEvent");
